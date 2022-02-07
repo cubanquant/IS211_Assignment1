325c31b7a23978dd36fe847657578a49119b2756
kellylanahan <78877452+kellylanahan@users.noreply.github.com>
2022-02-06 20:19:35 -0500

    Add files via upload

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,18 @@
-
 class Book:
+    author = ''
+    title = ''
+
     def __init__(self, author, title):
-        pass
-    
+        self.author = author
+        self.title = title
+
     def display(self):
-        pass
+        print(self.title + ", written by " + self.author)
 
 
-if __name__ == "__main__":
-    pass
+obj1 = Book('John Steinbeck', 'Of Mice and Men')
+obj2 = Book('Harper Lee', 'To Kill a Mockingbird')
+
+obj1.display()
+print()
+obj2.display()

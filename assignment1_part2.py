34d88763b458de8985a8ad480d276ffe0d443ca3
Bigmack799 <roberto.mack@yahoo.com>
2026-09-05 13:19:57 -0400

    Completed assignment 1 part 2

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,12 +1,21 @@
+# Bigmack799 Roberto Mack
 
 class Book:
+    author = ""
+    title = ""
+
     def __init__(self, author, title):
-        pass
+        self.author = author
+        self.title = title
     
     def display(self):
-        pass
+        print(f"{self.title}, written by {self.author}")
 
 
 if __name__ == "__main__":
     a = Book("Thorpe", "Beat the Dealer")
+
+    b = Book("Walter Scott", "Ivanhoe")
+    a.display()
+    b.display()
     print("This is a assignment 1 part 2")
